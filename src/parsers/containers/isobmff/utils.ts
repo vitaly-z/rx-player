bfac36fdee5fb680f99acd8fbb43c6781d9f772c
peaBerberian <pea.berberian@gmail.com>
2019-12-09 16:49:54 +0100

    parsers/isobmff: last range from sidx goes to infinity

--- src/parsers/containers/isobmff/utils.ts
+++ src/parsers/containers/isobmff/utils.ts
@@ -174,6 +174,13 @@ function getSegmentsFromSidx(
     offset += refSize;
   }
 
+  if (segments.length > 0) {
+    const lastSegment = segments[segments.length - 1];
+    if (lastSegment !== undefined && lastSegment.range !== undefined) {
+      lastSegment.range[1] = Infinity;
+    }
+  }
+
   return segments;
 }
 
