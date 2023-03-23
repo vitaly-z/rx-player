c682fecf0b4332093f0619b919f732b324df056d
Paul Berberian <pea.berberian@gmail.com>
2023-03-23 16:45:48 +0100

    wip

--- src/core/init/utils/media_duration_updater.ts
+++ src/core/init/utils/media_duration_updater.ts
@@ -180,11 +180,6 @@ function setMediaSourceDuration(
     } else {
       newDuration = manifest.getMaximumSafePosition();
     }
-  } else if (manifest.isDynamic) {
-    const livePosition = manifest.getLivePosition();
-    if (livePosition !== undefined) {
-      newDuration = Math.max(livePosition, newDuration);
-    }
   }
 
   if (manifest.isDynamic) {
